3216e02da3991e8a97b0286841f52125fe694b7a
Kiran M P <93343512+Kiran-M-P@users.noreply.github.com>
2022-01-28 22:26:41 +0530

    Add files via upload

---
+++ commands.sh
@@ -0,0 +1,19 @@
+#COMMANDS
+
+# ls === list (all the items inside a dir)
+# cd === change directory
+# cd ~ === back to root dir
+# cd .. === one level back
+# clear === cleans cmd
+# mkdir === create folder
+# touch === create file
+# start === open file
+# code === opens file in vsCode
+# rm === delete file
+# pwd === prints current dir location
+# rm * === removes all files in a dir
+# rm -r === remove dir
+
+#SHORTCUTS 
+# arrow up, down === cycles past commands
+# alt R,L === gets to end of a word
